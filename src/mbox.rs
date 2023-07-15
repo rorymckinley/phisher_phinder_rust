fa3a3f083b05adfe51ec48738cd5bbf70f03b670
Rory McKinley <rorymckinley@gmail.com>
2023-07-15 17:25:25 +0200

    Parse GMail takeout mbox

---
+++ src/mbox.rs
@@ -0,0 +1,91 @@
+use regex::Regex;
+
+#[cfg(test)]
+mod mbox_parse_tests {
+    use super::*;
+
+    #[test]
+    fn splits_string_on_from_marker() {
+        assert_eq!(expected(), parse(&input()))
+    }
+
+    #[test]
+    fn deals_with_broken_mail_body() {
+        assert_eq!(expected(), parse(&input_with_broken_entry()))
+    }
+
+    fn input() -> String {
+        format!("{}\r\n{}\r\n{}", entry_1(), entry_2(), entry_3())
+    }
+
+    fn input_with_broken_entry() -> String {
+        format!("{}\r\n{}\r\n{}\r\n{}", entry_1(), broken_entry(), entry_2(), entry_3())
+    }
+
+    fn expected() -> Vec<String> {
+        let mail1 = mail_1();
+        let mail2 = mail_2();
+        let mail3 = mail_3();
+
+        vec![mail1, mail2, mail3]
+    }
+
+    fn entry_1() -> String {
+        format!("From 123@xxx Sun Jun 11 20:53:34 +0000 2023\r\n{}", mail_1())
+    }
+
+    fn entry_2() -> String {
+        format!("From 123@xxx Sun Jun 11 20:53:35 +0000 2023\r\n{}", mail_2())
+    }
+
+    fn entry_3() -> String {
+        format!("From 123@xxx Sun Jun 11 20:53:36 +0000 2023\r\n{}", mail_3())
+    }
+
+    fn broken_entry() -> String {
+        format!("From 123@xxx Sun Jun 11 20:53:35 +0000 2023\r\n{}", broken_mail())
+    }
+
+    fn mail_1() -> String {
+"Delivered-To: victim1@test.zzz\r
+From: scammer@dodgy.zzz\r
+Subject: Dodgy Subject 1\r
+\r
+blahblah".into()
+    }
+
+    fn mail_2() -> String {
+"Delivered-To: victim2@test.zzz\r
+From: scammer@dodgy.zzz\r
+Subject: Dodgy Subject 2\r
+\r
+>From the very beginning our goal".into()
+    }
+
+    fn mail_3() -> String {
+"Delivered-To: victim3@test.zzz\r
+From: scammer@dodgy.zzz\r
+Subject: Dodgy Subject 1\r
+\r
+blahblah".into()
+    }
+
+    fn broken_mail() -> String {
+"Delivered-To-Not: victim3@test.zzz\r
+From: scammer@dodgy.zzz\r
+Subject: Dodgy Subject 1\r
+\r
+blahblah".into()
+    }
+}
+
+pub fn parse(mbox_contents: &str) -> Vec<&str> {
+    let re = Regex::new(r"(?ms).+?(Delivered-To:.+)\z").unwrap();
+
+    mbox_contents
+        .split("\r\nFrom ")
+        .filter_map(|snippet| {
+            re.captures(snippet).map(|caps| caps.get(1).unwrap().as_str())
+        })
+        .collect()
+}
---
+++ tests/mbox_parsing_integration_tests.rs
@@ -0,0 +1,48 @@
+use assert_cmd::Command;
+use assert_json_diff::assert_json_eq;
+use serde_json::{json, Value};
+
+#[test]
+fn parses_mbox_file_test() {
+    let mut cmd = Command::cargo_bin("pp-mbox-parser").unwrap();
+
+    let assert = cmd
+        .write_stdin(input())
+        .assert()
+        .success();
+    let json_utf8 = &assert
+        .get_output()
+        .stdout;
+
+    let json_data: serde_json::Value = serde_json::from_str(
+        std::str::from_utf8(json_utf8).unwrap()
+    ).unwrap();
+
+    assert_json_eq!(expected_json(), json_data);
+}
+
+fn input() -> String {
+    format!("{}\r\n{}", entry_1(), entry_2())
+}
+
+fn entry_1() -> String {
+    format!("From 123@xxx Sun Jun 11 20:53:34 +0000 2023\r\n{}", mail_body_1())
+}
+
+fn entry_2() -> String {
+    format!("From 456@xxx Sun Jun 11 20:53:35 +0000 2023\r\n{}", mail_body_2())
+}
+
+fn mail_body_1() -> String {
+"Delivered-To: victim1@test.zzz\r
+Subject: Dodgy Subject 1".into()
+}
+
+fn mail_body_2() -> String {
+"Delivered-To: victim2@test.zzz\r
+Subject: Dodgy Subject 2".into()
+}
+
+fn expected_json() -> Value {
+    json!([mail_body_1(), mail_body_2()])
+}
